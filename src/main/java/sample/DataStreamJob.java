c5bdd3e5fe7888eb3c784461f0744f3179c310d1
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2023-09-25 20:13:07 +0300

    refactor: minor readability cleanup.

--- src/main/java/sample/DataStreamJob.java
+++ src/main/java/sample/DataStreamJob.java
@@ -1,19 +1,31 @@
 package sample;
 
 import java.util.Optional;
+import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.connector.source.Source;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
 import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import sample.doman.Book;
 import sample.operators.BookDeserializer;
 import sample.sinks.BookJdbcSink;
 import sample.sources.BookFileSource;
 
 public class DataStreamJob {
   private static final Logger LOGGER = LogManager.getLogger();
+
+  private final Source<String, ?, ?> source;
+  private final SinkFunction<Book> sink;
+
+  public DataStreamJob(Source<String, ?, ?> source, SinkFunction<Book> sink) {
+    this.source = source;
+    this.sink = sink;
+  }
 
   public static void main(String[] args) throws Exception {
     ParameterTool params = ParameterTool.fromArgs(args);
@@ -23,36 +35,43 @@ public class DataStreamJob {
         .getExecutionEnvironment();
     // make parameters available in the web interface
     env.getConfig().setGlobalJobParameters(params);
-    // env.disableOperatorChaining();
+
+    DataStreamJob job = new DataStreamJob(
+        BookFileSource.forTextLineInputFormat(
+            Optional.ofNullable(params.get("input-dir")).orElse("./"),
+            ".json.gz"),
+        BookJdbcSink.sink(
+            JdbcExecutionOptions.builder()
+                .withBatchSize(100)
+                .withBatchIntervalMs(200)
+                .withMaxRetries(5)
+                .build(),
+            new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                .withUrl(
+                    Optional.ofNullable(params.get("db-url"))
+                        .orElse("jdbc:postgresql://localhost:5432/books"))
+                .withDriverName("org.postgresql.Driver")
+                .withUsername("postgres")
+                .build()
+        )
+    );
+
+    LOGGER.info("execute");
+    JobExecutionResult result = job.execute(env);
+    LOGGER.info("done, {} ms", result.getNetRuntime());
+  }
+
+  public JobExecutionResult execute(
+      StreamExecutionEnvironment env) throws Exception {
     env.fromSource(
-            BookFileSource.forTextLineInputFormat(
-                Optional.ofNullable(params.get("input-dir")).orElse("./"),
-                ".json.gz"),
+            source,
             WatermarkStrategy.noWatermarks(),
-            "file input")
+            "read source")
         .rebalance()
         .map(new BookDeserializer())
         .name("parse book from a json line")
-        .addSink(
-            BookJdbcSink.sink(
-                JdbcExecutionOptions.builder()
-                    .withBatchSize(100)
-                    .withBatchIntervalMs(200)
-                    .withMaxRetries(5)
-                    .build(),
-                new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
-                    .withUrl(
-                        Optional.ofNullable(params.get("db-url"))
-                            .orElse("jdbc:postgresql://localhost:5432/books"))
-                    .withDriverName("org.postgresql.Driver")
-                    .withUsername("postgres")
-                    .build()
-            ))
-        .name("persist to db");
-
-    LOGGER.info("executing");
-    env.execute("Sample Books ETL Job");
-
-    LOGGER.info("done");
+        .addSink(sink)
+        .name("persist to storage");
+    return env.execute("Sample Books ETL Job");
   }
 }
--- src/main/java/sample/sources/BookFileSource.java
+++ src/main/java/sample/sources/BookFileSource.java
@@ -13,12 +13,14 @@ public final class BookFileSource {
 
   public static FileSource<String> forTextLineInputFormat(
       String rootPath,
-      String ...extensions) throws IOException {
+      String... extensions) throws IOException {
     LOGGER.info("root: {}, extensions: {}", rootPath, extensions);
+
     Path[] paths = new PathScanner(extensions)
         .scan(new Path(rootPath))
         .toArray(Path[]::new);
     LOGGER.info("paths found: {}", paths.length);
+
     return FileSource.forRecordStreamFormat(
             new TextLineInputFormat(),
             paths)
