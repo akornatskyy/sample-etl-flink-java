06ed8789edf10ef6f317e2427a329dcf744a66fd
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2023-10-01 20:51:49 +0300

    refactor: basic book ingestion readability.

--- src/main/java/sample/basic/BasicBooksIngestion.java
+++ src/main/java/sample/basic/BasicBooksIngestion.java
@@ -1,77 +1,114 @@
 package sample.basic;
 
+import java.io.IOException;
 import java.util.Optional;
 import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.connector.source.Source;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
 import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
 import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import sample.basic.domain.Book;
 import sample.basic.operators.BookDeserializer;
+import sample.shared.fs.FileExtensionFilter;
+import sample.shared.fs.PathScanner;
 import sample.basic.sinks.BookJdbcSink;
-import sample.basic.sources.BookFileSource;
 
 public final class BasicBooksIngestion {
   private static final Logger LOGGER = LogManager.getLogger();
 
-  private final Source<String, ?, ?> source;
-  private final SinkFunction<Book> sink;
+  private final BasicBooksIngestion.Params params;
 
-  public BasicBooksIngestion(Source<String, ?, ?> source, SinkFunction<Book> sink) {
-    this.source = source;
-    this.sink = sink;
+  public BasicBooksIngestion(ParameterTool params) {
+    this.params = new Params(params);
   }
 
   public static void main(String[] args) throws Exception {
-    ParameterTool params = ParameterTool.fromArgs(args);
-    LOGGER.info("started, params: {}", params.toMap());
-
     StreamExecutionEnvironment env = StreamExecutionEnvironment
         .getExecutionEnvironment();
-    // make parameters available in the web interface
-    env.getConfig().setGlobalJobParameters(params);
 
-    BasicBooksIngestion job = new BasicBooksIngestion(
-        BookFileSource.forTextLineInputFormat(
-            Optional.ofNullable(params.get("input-dir")).orElse("./"),
-            ".json.gz"),
-        BookJdbcSink.sink(
-            JdbcExecutionOptions.builder()
-                .withBatchSize(100)
-                .withBatchIntervalMs(200)
-                .withMaxRetries(5)
-                .build(),
-            new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
-                .withUrl(
-                    Optional.ofNullable(params.get("db-url"))
-                        .orElse("jdbc:postgresql://localhost:5432/books"))
-                .withDriverName("org.postgresql.Driver")
-                .withUsername("postgres")
-                .build()
-        )
-    );
+    addBooksIngestion(env, ParameterTool.fromArgs(args));
 
     LOGGER.info("execute");
-    JobExecutionResult result = job.execute(env);
+    JobExecutionResult result = env.execute("Sample Books Basic ETL Job");
     LOGGER.info("done, {} ms", result.getNetRuntime());
   }
 
-  public JobExecutionResult execute(
-      StreamExecutionEnvironment env) throws Exception {
-    env.fromSource(
-            source,
-            WatermarkStrategy.noWatermarks(),
-            "read source")
-        .rebalance()
+  public static void addBooksIngestion(
+      StreamExecutionEnvironment env, ParameterTool params) throws IOException {
+    LOGGER.info("params: {}", params.toMap());
+    // make parameters available in the web interface
+    env.getConfig().setGlobalJobParameters(params);
+
+    BasicBooksIngestion ingestion = new BasicBooksIngestion(params);
+    ingestion.addBooksIngestion(env);
+  }
+
+  public static void addBooksIngestion(
+      SingleOutputStreamOperator<String> source,
+      SinkFunction<Book> sink) {
+    source.rebalance()
         .map(new BookDeserializer())
         .name("parse book from a json line")
         .addSink(sink)
         .name("persist to storage");
-    return env.execute("Sample Books ETL Job");
+  }
+
+  void addBooksIngestion(
+      StreamExecutionEnvironment env) throws IOException {
+    addBooksIngestion(
+        createDataStreamSource(env),
+        createSinkFunction());
+  }
+
+  DataStreamSource<String> createDataStreamSource(
+      StreamExecutionEnvironment env) throws IOException {
+    PathScanner pathScanner = new PathScanner(
+        new FileExtensionFilter(".json.gz"));
+    Path[] paths = pathScanner
+        .scan(new Path(params.inputDir))
+        .toArray(new Path[0]);
+    LOGGER.info("paths found: {}", paths.length);
+    return env.fromSource(
+        FileSource.forRecordStreamFormat(
+                new TextLineInputFormat(),
+                paths)
+            .build(),
+        WatermarkStrategy.noWatermarks(),
+        "read source");
+  }
+
+  SinkFunction<Book> createSinkFunction() {
+    return BookJdbcSink.sink(
+        JdbcExecutionOptions.builder()
+            .withBatchSize(100)
+            .withBatchIntervalMs(200)
+            .withMaxRetries(5)
+            .build(),
+        new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+            .withUrl(params.dbUrl)
+            .withDriverName("org.postgresql.Driver")
+            .withUsername("postgres")
+            .build());
+  }
+
+  static class Params {
+    public final String inputDir;
+    public final String dbUrl;
+
+    Params(ParameterTool params) {
+      inputDir = Optional.ofNullable(params.get("input-dir"))
+          .orElse("./");
+      dbUrl = Optional.ofNullable(params.get("db-url"))
+          .orElse("jdbc:postgresql://localhost:5432/books");
+    }
   }
 }
