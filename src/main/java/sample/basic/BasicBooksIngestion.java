2b0687f4a3e7f5f81305c45d7fa598ef736ef54e
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2024-02-02 20:27:08 +0200

    fix: avoid blocking execution in standalone mode.

--- src/main/java/sample/basic/BasicBooksIngestion.java
+++ src/main/java/sample/basic/BasicBooksIngestion.java
@@ -1,15 +1,10 @@
 package sample.basic;
 
-import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import sample.basic.streams.BooksIngestionStream;
 
 public final class BasicBooksIngestion {
-  private static final Logger LOGGER = LogManager.getLogger();
-
   public static void main(String[] args) throws Exception {
     StreamExecutionEnvironment env = StreamExecutionEnvironment
         .getExecutionEnvironment();
@@ -22,8 +17,6 @@ public final class BasicBooksIngestion {
         .getStream(BooksIngestionStream.Options.fromArgs(args))
         .accept(env);
 
-    LOGGER.info("execute");
-    JobExecutionResult result = env.execute("Sample Books Basic ETL Job");
-    LOGGER.info("done, {} ms", result.getNetRuntime());
+    env.execute("Sample Books Basic ETL Job");
   }
 }
